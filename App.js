bd8a384fec0045c0dd66d5cbf3d723a57a556ff0
kukuh <adjiferdinantara31@gmail.com>
2018-08-23 14:00:50 +0700

    haihai20

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Nama Kukuh Adji Ferdinantara</Text>
+        <Text>Kelas XI RPL 4</Text>
+        <Text>NO. 20</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+});
